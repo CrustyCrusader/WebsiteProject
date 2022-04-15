defc323f310e74b51ef065390b559071d759d786
Crusty <christopher.cruz.2015@gmail.com>
2022-04-14 21:27:10 -0400

    fixed some typos

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 db = SQLAlchemy(app)
 
 class users(db.Model):
-    _id = db.Colum(db.Interger, primary_key=True)
-    name = db.Colum( db.String(100))
-    email = db.Colum(db.String(100))
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column( db.String(100))
+    email = db.Column(db.String(100))
 
     def __init__(self, name, email):
             self.name = name
@@ -25,13 +25,27 @@ class users(db.Model):
 def home():
     return render_template("index.html")
 
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
 @app.route("/login", methods = ["POST", "GET"])
 def login():
     if request.method == "POST":
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
-        flash("You have logged in")
+
+        found_user = users.query.filter_by(name=user).first()
+
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user,"")
+            db.session.add(usr)
+            db.session.commit()
+
+        flash("Login Succesful!")
         return redirect(url_for("user"))
     else:
         if "user" in session:
@@ -49,6 +63,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email=email
+            db.session.commit()
             flash("Email saved")
         else:
             if "email" in session:
